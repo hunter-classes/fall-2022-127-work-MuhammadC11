f5fd69ba2f09de99ab26e25bacf8bf5f713c6b4f
Muhammad Chaudhry <71554327+MuhammadC11@users.noreply.github.com>
2022-09-01 14:06:20 +0000

    t2

--- turtles/t2.py
+++ turtles/t2.py
@@ -5,17 +5,9 @@ wn = turtle.Screen()
 crush = turtle.Turtle()
 
 # draw a square
-crush.forward(50)
-crush.right(90)
-
-crush.forward(50)
-crush.right(90)
-
-crush.forward(50)
-crush.right(90)
-
-crush.forward(50)
-crush.right(90)
+for i in range(4):
+  crush.forward(50)
+  crush.right(90)
 
 squirt = turtle.Turtle()
 squirt.up()
