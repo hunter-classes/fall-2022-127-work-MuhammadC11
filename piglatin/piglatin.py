fa51b6d5a6084fff4c25f81f934e18dc016d1e0b
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2022-09-15 21:10:32 -0400

    updated piglatin removing inputs

--- piglatin/piglatin.py
+++ piglatin/piglatin.py
@@ -1,12 +1,12 @@
 def bondify(name):
-  name = input("Input your first and last name")
-  space_location = name.find(" ")
-  last = name[space_location+1].upper() + name[space_location+2 :]
-  first = name[0].upper() + name[1:space_location]
-  finalName= last +", " + first + " " + last
-  return finalName
+    space_location = name.find(" ")
+    last = name[space_location+1].upper() + name[space_location+2:]
+    first = name[0].upper() + name[1:space_location]
+    finalName = last + ", " + first + " " + last
+    return finalName
 
-print(bondify("Mo"))
+
+print(bondify("Muhammad Chaudhry"))
 
 
 """
@@ -23,14 +23,16 @@ try to also handle upper case words
 
 """
 
+
 def piglatin(word):
-  low = word.lower()
-  
-  if low[0] == "a" or low[0] == "e" or low[0] == "i"  or low[0] == "u"  or low[0] == "y":
-    return low + "yay"
-  
-  else:
-      first_letter = low[0]
-      return low[1:] + first_letter + "ay"
-     
+    low = word.lower()
+
+    if low[0] == "a" or low[0] == "e" or low[0] == "i" or low[0] == "u" or low[0] == "y":
+        return low + "yay"
+
+    else:
+        first_letter = low[0]
+        return low[1:] + first_letter + "ay"
+
+
 print(piglatin("ello bruv"))
