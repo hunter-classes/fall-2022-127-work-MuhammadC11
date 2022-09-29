3330c7754a218fe4e8c329f148097b446f5300b7
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2022-09-29 08:42:35 -0400

    added function notation

--- piglatin/fizzbuzz.py
+++ piglatin/fizzbuzz.py
@@ -1,9 +1,13 @@
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0 and i % 5 != 0:
-        print("Fizz")
-    elif i % 5 == 0 and i % 3 != 0:
-        print("Buzz")
-    else:
-        print(i)
+def fizzbuzz(n):
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+fizzbuzz(90)
