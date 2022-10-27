757ab03de298511cfb71cec19c0dbc566134c414
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2022-10-27 12:14:26 -0400

    added professors solution

--- mode/modes.py
+++ mode/modes.py
@@ -21,3 +21,33 @@ def freq(l, v):
 
 print("the value", number, "appears", freq(
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], number), "times")
+
+
+def mode(dataset):
+    """
+    returns the mode of a dataset
+    """
+    mode = None
+    maxCount = 0
+    for value in dataset:
+        frequency = dataset.count(value)
+        if frequency > maxCount:
+            maxCount = frequency
+            mode = value
+    return mode
+
+
+print("the mode is", mode([4, 4, 4, 4, 4, 4, 5, 6, 6, 7, 8, 6, 9, 10]), )
+
+
+def modev2(dataset):
+    modeSoFar = dataset[0]
+    freqSoFar = freq(dataset, modeSoFar)
+    for item in dataset[1:]:
+        if freq(dataset, item) > freqSoFar:
+            modeSoFar = item
+            freqSoFar = freq(dataset, item)
+    return modeSoFar
+
+
+print("the mode is", mode([4, 6, 67, 8, 9, 9, 10, 11, 12]))
