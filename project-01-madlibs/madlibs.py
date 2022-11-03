766e2c3a1f6ff52cfbf9de2917a76a71563e42a5
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2022-11-03 00:04:23 -0400

    fixed up madlibs and added capitalization method

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -1,23 +1,35 @@
+# I did the import file extra which is evident when i use open("").
+# I did the constnant character extra: character = random.choice(character)
+#story = story.replace("<character>", character)
+# I did the extra to fix capitalization: if i.find(".") != -1:
+#nextWord = storyData[storyData.index(i) + 1]
+# storyData[storyData.index(
+# i) + 1] = nextWord.capitalize()
+
+
 import random
 f = open("story.dat", "r")
 n = open("nouns.dat", "r")
 v = open("verbs.dat", "r")
 a = open("adjectives.dat", "r")
+c = open("characters.dat", "r")
 storyData = f.read()  # read the entire file into a string
 nounData = n.read()  # read the entire file into a string
 adjectiveData = a.read()  # read the entire file into a string
 verbData = v.read()  # read the entire file into a string
+characterData = c.read()
 # shoutout to Thomas H. for showing me I needed to split the data into a list of strings
 nounList = nounData.split()
 # shoutout to Thomas H. for showing me I needed to split the data into a list of strings
 verbList = verbData.split()
 # shoutout to Thomas H. for showing me I needed to split the data into a list of strings
 adjectiveList = adjectiveData.split()
+characterList = characterData.split()
 
 
-def madLib(story, noun, adjective, verb):
+def madLib(story, character, noun, adjective, verb):
     # make character a constant variable chosen at random at the beginning of the function call. This way the character is the same throughout the story.
-    character = random.choice(noun)
+    character = random.choice(character)
     story = story.replace("<character>", character)
     # split the string into a list of strings so that we can iterate through it to find each madlib placeholder.
     storyData = story.split()
@@ -31,10 +43,14 @@ def madLib(story, noun, adjective, verb):
         elif i.find("<verb>") != -1:  # if there is any occurence of the word <verb>
             storyData[storyData.index(i)] = random.choice(
                 verb)  # replace the word with a random verb
+        if i.find(".") != -1:
+            nextWord = storyData[storyData.index(i) + 1]
+            storyData[storyData.index(
+                i) + 1] = nextWord.capitalize()
     return " ".join(storyData)  # join the list of words into a string
 
 
-print(madLib(storyData, nounList, adjectiveList, verbList))
+print(madLib(storyData, characterList, nounList, adjectiveList, verbList))
 
 
 # this is my old code that i want to leave here in case i need to reference it later
